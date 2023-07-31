590b456377e13c6377e3bf4b0f6ae15b0b81bfd7
구너훈 <sjm601@naver.com>
2023-07-31 15:17:54 +0900

    V2

--- src/HelloGit.java
+++ src/HelloGit.java
@@ -3,5 +3,6 @@ public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("1111111111111111111111111111");
 		System.out.println("우주최강 아스날 외데고르 사랑해");
+		System.out.println("세계 최고 귀여미 부카요 사카 ");
 	}
 }

72e150e0628ff29e7e35f8974b070ce82e11b51c
구너훈 <sjm601@naver.com>
2023-07-31 16:40:01 +0900

    homework

--- src/HelloGit.java
+++ src/HelloGit.java
@@ -4,5 +4,6 @@ public class HelloGit {
 		System.out.println("1111111111111111111111111111");
 		System.out.println("우주최강 아스날 외데고르 사랑해");
 		System.out.println("3333333333333333333333333333333333333333");
+		System.out.println("집에서 작업을 하였습니다!!");
 	}
 }
